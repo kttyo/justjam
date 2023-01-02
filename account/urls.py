ede808f0e64fb0aa7476eb317f622a13af2aed42
kttyo <16376462+kttyo@users.noreply.github.com>
2023-01-02 11:37:34 +0900

    Added a user pattern for user-status view.

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 urlpatterns = [
     path('login', views.login_view, name="login"),
     path('logout', views.logout_view, name="logout"),
+    path('user-status', views.user_status, name="user-status"),
 ]
