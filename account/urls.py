195d2b70a85b77b374159d548512408fe586bfb3
kttyo <16376462+kttyo@users.noreply.github.com>
2023-01-18 14:30:59 +0900

    Added a path for signup.

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login', views.login_view, name="login"),
     path('logout', views.logout_view, name="logout"),
     path('user-status', views.user_status, name="user-status"),
+    path('signup', views.signup_view, name="signup"),
 ]
