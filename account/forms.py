a8a3778a83efe982c500289d5c990451da41fe4c
kttyo <16376462+kttyo@users.noreply.github.com>
2023-01-18 16:58:31 +0900

    Created CustomUserCreationForm.

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields
