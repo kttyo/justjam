6d0f2267a32f684e642d2e41c0af5a13ca9c2491
kttyo <16376462+kttyo@users.noreply.github.com>
2023-02-13 15:51:30 +0900

    Made a change to better handle invalid requests in the login page.

--- account/views.py
+++ account/views.py
@@ -8,6 +8,10 @@ from .music_user_token import get_music_user_token
 import logging
 logger = logging.getLogger(__name__)
 
+main_page_url = 'http://justjam.jppj.jp'
+if settings.DEBUG:
+    main_page_url = 'http://localhost:8080/html?env=development'
+
 
 def user_status(request):
     dict_data = {}
@@ -31,30 +35,39 @@ def login_view(request):
         form = LoginForm(request, data=request.POST)
 
         if form.is_valid():
+            logger.info('Form input was valid')
             user = form.get_user()
 
             if user:
                 login(request, user)
-                if settings.DEBUG:
-                    return redirect(to='http://localhost:8080/html?env=development')
-                else:
-                    return redirect(to='http://justjam.jppj.jp')
+                return redirect(to=main_page_url)
+            else:
+                logger.error('error happened')
+                context = {
+                    'form': LoginForm(),
+                    'main_page': main_page_url
+                }
+                return render(request, 'registration/login.html', context)
+        else:
+            logger.info('Form input was not valid')
+            context = {
+                'form': LoginForm(),
+                'main_page': main_page_url,
+                'error_message': 'Your input was not valid. Please try again.'
+            }
+            return render(request, 'registration/login.html', context)
 
     else:
-        context = {'form': LoginForm()}
-        if settings.DEBUG:
-            context['main_page'] = 'http://localhost:8080/html?env=development'
-        else:
-            context['main_page'] = 'http://justjam.jppj.jp'
+        context = {
+            'form': LoginForm(),
+            'main_page': main_page_url
+        }
         return render(request, 'registration/login.html', context)
 
 
 def logout_view(request):
     logout(request)
-    if settings.DEBUG:
-        return redirect(to='http://localhost:8080/html?env=development')
-    else:
-        return redirect(to='http://justjam.jppj.jp')
+    return redirect(to=main_page_url)
 
 
 def signup_view(request):
@@ -62,19 +75,13 @@ def signup_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            if settings.DEBUG:
-                return redirect(to='http://localhost:8080/html?env=development')
-            else:
-                return redirect(to='http://justjam.jppj.jp')
+            return redirect(to=main_page_url)
         else:
             context = {
                 'form': CustomUserCreationForm(),
-                'error_message': 'Some Error Happened'
+                'error_message': 'Some Error Happened',
+                'main_page': main_page_url
             }
-            if settings.DEBUG:
-                context['main_page'] = 'http://localhost:8080/html?env=development'
-            else:
-                context['main_page'] = 'http://justjam.jppj.jp'
             return render(request, 'registration/signup.html', context)
 
     else:
