251354458eca30fbc25d36d8be47a59a1be65dc9
kttyo <16376462+kttyo@users.noreply.github.com>
2024-12-31 11:00:38 +0900

    Remove obsolete url configuration for dev environment

--- justjam_proj/urls.py
+++ justjam_proj/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('api/', include('api.urls')),
-    path('', include('musickit.urls')),
+#    path('', include('musickit.urls')),
 ]
