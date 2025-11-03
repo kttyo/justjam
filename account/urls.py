fffe9b5460339660f6c1faa34b7715cefc31aad7
kttyo <16376462+kttyo@users.noreply.github.com>
2025-11-04 06:25:43 +0900

    add endpoints for simple jwt

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'account'
 urlpatterns = [
@@ -9,4 +12,6 @@ urlpatterns = [
     path('user-status', views.user_status, name="user-status"),
     path('music-user-token', views.music_user_token, name="music-user-token"),
     path('signup', views.signup_view, name="signup"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
