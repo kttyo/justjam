b485e1ee4b0c973fd457ea8455232cad2bfa61a6
kttyo <16376462+kttyo@users.noreply.github.com>
2025-12-12 14:15:54 +0900

    add loopa.jppj.jp in settings.py

--- justjam_proj/settings.py
+++ justjam_proj/settings.py
@@ -4,8 +4,8 @@ ALLOWED_HOSTS = ['justjam.jppj.jp']
 
 STATIC_ROOT = '/var/www/jppj.jp/justjam/html/static/'
 
-CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp']
+CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp']
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp']
+CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp']
