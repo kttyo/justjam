cba6c1d41af9a6031af5d9013422ed9571192f66
kttyo <16376462+kttyo@users.noreply.github.com>
2026-01-18 22:03:15 +0900

    allow loopamusic.com domain

--- justjam_proj/settings.py
+++ justjam_proj/settings.py
@@ -1,15 +1,15 @@
 from .settings_common import *
 
-ALLOWED_HOSTS = ['justjam.jppj.jp', 'loopa.jppj.jp']
+ALLOWED_HOSTS = ['justjam.jppj.jp', 'loopa.jppj.jp', 'loopamusic.com']
 
 STATIC_ROOT = '/var/www/jppj.jp/justjam/html/static/'
 
-CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp']
+CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp','loopamusic.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp']
+CSRF_TRUSTED_ORIGINS = ['https://justjam.jppj.jp','https://loopa.jppj.jp','loopamusic.com']
 
 CORS_ALLOWED_ORIGINS = [
-    'https://loopa.jppj.jp',
+    'https://loopa.jppj.jp','loopamusic.com'
 ]
